4ec53d2f597c3db2c6016974978e915931482c26
Cleiton Capristano <cleiton.capristano@gmail.com>
2022-06-11 10:41:10 -0300

    atividade depois da eula e programinha de sorteio

---
+++ api-aula/src/br/com/aula/atividades/Atividade3.java
@@ -0,0 +1,25 @@
+package br.com.aula.atividades;
+
+public class Atividade3 {
+	
+	//Colete a quantidade de alunos de uma turma
+	//Colete o nome de cada um
+	//Colete a média do aluno para cada trimestre de cada matéria, ou seja 4 notas para cada materia, para cada um dos 4 periodos do ano
+	//As materias podem estar fixa no código fonte, não precisa seguir um padrão de quantidade de materias e nomes especificos para cada uma delas
+	
+	//Diga se o aluno está aprovado, reprovado ou em recuperação
+	//Como funciona nossa metrica
+	//Aprovado média 7
+	//Recuperação média >= 6,25
+	//Reprovado < 6,25
+	
+	//Você deve usar concatenação, variáveis String, int e double
+	//deve usar if, swtich, array e looping for
+	
+	//concluindo a atividade subir ela para seu repositório do Git
+	//cada Aluno poderá ter seu código analisado em uma call privada depois da aula (de forma randon)
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ api-aula/src/br/com/aula/atividades/EscolherAluno.java
@@ -0,0 +1,13 @@
+package br.com.aula.atividades;
+
+import java.util.Random;
+
+public class EscolherAluno {
+	
+	public static void main(String[] args) {
+		String[] nome = {"Daniel", "Guilherme", "Luiz", "Matheus"};
+		int i = new Random().nextInt(3);//numero random 0-3
+		System.out.println(nome[i]);
+	}
+
+}
