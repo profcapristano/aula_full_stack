ed817c6df66a99eedfddb1454b6017ed4bef8891
Cleiton Capristano <cleiton.capristano@gmail.com>
2022-06-25 08:37:39 -0300

    alteração de enunciado

--- api-aula/src/br/com/aula/atividades/Atividade4.java
+++ api-aula/src/br/com/aula/atividades/Atividade4.java
@@ -3,7 +3,7 @@ package br.com.aula.atividades;
 
 //entrevista de emprego
 //o sistema deve aceitar um numero N de canditatos
-//caso o candidato não tenha nenhuma habilidade de progração conveniente para a empresa e não tenha vontade de aprender, o sistema não deve proseguir com perguntas
+//coletar o nome de cada candidato
 
 //o sistema deve perguntar, se o candidato sabe Java ou C++
 //caso o candidato sabe ao menos uma dessas linguagens ele terá o titulo Programador Backend (na empresa X que utiliza apenas essas linguagens de back)
@@ -11,14 +11,14 @@ package br.com.aula.atividades;
 //o sistema deve pergunta, se o candidato sabe React ou Angular
 //caso o candidato sabe ao menos uma dessas linguagens ele terá o titulo Programador Frontend (na empresa X que utiliza apenas essas linguagens de front)
 
+//caso o candidato não tenha nenhuma habilidade de progração conveniente para a empresa e não tenha vontade de aprender, o sistema não deve proseguir com perguntas
 //caso o candidato não tenha habilidade de programação mas queira aprender, ele terá o titulo de Aprediz de Desenvolvimento, então o sistema não continua com as demais peguntas
 
 //se o candidato tiver o titulo de Programador Backend e Programador Frontend então ele deve ter o titulo de Programador Full Stack
 
 //o sistema deve perguntar se o candidato se comunica bem com as pessoas
 //caso o candidato se comunique bem e tenha o titulo de Full Stack, então ele deve ter o titulo de Tech Lead 
-
-
+//impriva o nome e cargo de candidato, caso ele não tenha nome e cargo o sistema deve imprimir que o candidato não foi aprovado
 public class Atividade4 {
 
 }
